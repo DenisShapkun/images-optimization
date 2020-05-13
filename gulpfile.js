087ce90897a1cccc14fe8a30f4658b58a4c18d07
Shapkun Denis <shapkun@trafficdna.com>
2020-05-13 22:16:34 +0300

    images-optimization

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp         = require('gulp'),
+    imageResize  = require('gulp-image-resize');
+    imagemin     = require('gulp-imagemin'),
+    imgCompress  = require('imagemin-jpeg-recompress'),
+    del          = require('del'),
+    cache        = require('gulp-cache');
+
+
+// Resize images
+// need to test
+gulp.task('resize', function () {
+  gulp.src('app/images/original/**/*')
+    .pipe(imageResize({
+      width: 1000,
+      height: 1000,
+      crop: true,
+      upscale: false
+    }))
+    .pipe(gulp.dest('app/images/resized'));
+});
+
+
+// Optimize images
+gulp.task('compress', function() {
+  return gulp.src('app/images/**/*')
+  .pipe(cache(imagemin([
+    imgCompress({
+      loops: 4,
+      min: 60,
+      max: 70,
+      quality: 'high' 
+    }),
+    imagemin.gifsicle(),
+    imagemin.optipng(),
+    imagemin.svgo()
+  ])))
+  .pipe(gulp.dest('dist/images'));
+});
+
+
+// Cleaning Production distributive
+gulp.task('clean', function(done) {
+  del.sync('dist');
+  done();
+});
+
+
+// Clear Cache
+gulp.task('clear', function() {
+  return cache.clearAll();
+});
+
+
+// Build Production distributive with all updates
+gulp.task('default', gulp.series('clean', 'compress'));
