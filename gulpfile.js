fce04f801a63c9019bd22bb3743d0c8bf23f8958
Shapkun Denis <shapkun@trafficdna.com>
2022-08-27 03:28:53 +0300

    fix parametres

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,54 @@
 var gulp                   = require('gulp'),
-    imagemin               = require('gulp-imagemin'),
+    // imagemin               = require('gulp-imagemin'),
+    imagemin               = require('gulp-imagemin-fix'),
     imageminJpegRecompress = require('imagemin-jpeg-recompress'),
     imageminPngquant       = require('imagemin-pngquant'),
     imageResize            = require('gulp-image-resize');
     del                    = require('del'),
-    cache                  = require('gulp-cache');
+    cache                  = require('gulp-cache'),
+    parallel               = require("concurrent-transform"),
+    os                     = require("os"),
+    plumber                = require('gulp-plumber');
 
 // Resize images
 // need to test
 gulp.task('resize', function () {
-  return gulp.src('app/images/original/**/*')
-    .pipe(imageResize({
-      width: 1000,
-      height: 1000,
-      crop: true,
+  return gulp.src('app/images/original/**/*.{jpg,jpeg,png,gif}')
+    .pipe(parallel(
+      imageResize({
+      width: 920,
+      //height: 1080,
+      crop: false,
       upscale: false,
-      //format : 'jpeg' // Converting to jpeg
-    }))
+      //format : 'jpg' // Converting to jpeg
+    }),
+    os.cpus().length
+    ))
     .pipe(gulp.dest('app/images/resized'));
 });
 
 // Optimize images
 gulp.task('compress', function() {
   return gulp.src('app/images/**/*')
+  //.pipe(plumber())
   .pipe(cache(imagemin([
     imageminJpegRecompress({
       loops: 4,
-      min: 60,
-      max: 70,
+      min: 70,
+      max: 80,
       quality: 'high' 
     }),
     imageminPngquant({
-      quality: [0.6, 0.7]
+      quality: [0.7, 0.8]
     }),
     imagemin.gifsicle({
       optimizationLevel: 3,
       interlaced: true
     }),
     imagemin.svgo()
-  ])))
+  ], {
+    //verbose: true
+  })))
   .pipe(gulp.dest('dist/images'));
 });
 
@@ -55,3 +65,4 @@ gulp.task('clear', function() {
 
 // Build Production distributive with all updates
 gulp.task('default', gulp.series('clean', 'compress'));
+// gulp.task('default', gulp.series('compress'));
